af39261a2c307cd7f0d4078516694797f4619690
visnuvishal777 <155134304+visnuvishal777@users.noreply.github.com>
2026-02-14 11:39:03 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,24 +1,4 @@
-variable "aws_region" {
-  type    = string
-  default = "us-east-1"
-}
-variable "instance_type" {
-  type    = string
-  default = "t3.micro"
-}
-variable "ami_id" {
-  type = string
-
-  validation {
-    condition     = length(trimspace(var.ami_id)) > 0
-    error_message = "ami-0b6c6ebed2801a5cb"
-  }
-}
-variable "existing_key_pair_name" {
-  type    = string
-  default = "devops"
-}
-variable "allowed_ssh_cidr" {
-  type    = string
-  default = "0.0.0.0/0"
-}
+variable "ami" {
+  description = "AMI ID for EC2"
+  type        = string
+} 
