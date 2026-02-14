1252cc0c4d0b4b80290e5833e4e3168861efc589
visnuvishal777 <155134304+visnuvishal777@users.noreply.github.com>
2026-02-14 12:13:44 +0530

    Add AWS variables and update AMI ID validation
    Added new variables for AWS region, instance type, and SSH CIDR. Updated AMI ID variable with validation.

--- variables.tf
+++ variables.tf
@@ -1,4 +1,24 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
 variable "ami_id" {
-  description = "AMI ID for EC2"
-  type        = string
-} 
+  type = string
+
+  validation {
+    condition     = length(trimspace(var.ami_id)) > 0
+    error_message = "ami-0b6c6ebed2801a5cb"
+  }
+}
+variable "existing_key_pair_name" {
+  type    = string
+  default = "proj1"
+}
+variable "allowed_ssh_cidr" {
+  type    = string
+  default = "0.0.0.0/0"
+}
